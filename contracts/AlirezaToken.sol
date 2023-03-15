9afdd187ffe9fba12641dc195676bf11f8cb7dc2
alireza alizadeh <alireza.aa9700@gmail.com>
2023-03-15 18:44:45 +0330

    customize

--- contracts/AlirezaToken.sol
+++ contracts/AlirezaToken.sol
@@ -5,8 +5,13 @@ pragma solidity 0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract AlirezaToken is ERC20 {
-    constructor(uint initialSupply) ERC20("AlirezaToken", "ALT") {
+    address payable public owner;
+    uint private initialSupply = 70_000_000;
+
+    constructor() ERC20("AlirezaToken", "ALT") {
+        owner = payable(msg.sender);
         // address of deployer, the amount
-        _mint(msg.sender, initialSupply);
+        // _mint(owner, initialSupply * (10 ** 18));
+        _mint(owner, initialSupply * (10 ** decimals()));
     }
 }
